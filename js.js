65eca70925f991cbaf3c6ccf4d6535d6c02e6773
jon2502 <jonbjarke25@outlook.dk>
2024-03-30 09:03:05 +0100

    Set up for flip function for double faced cards

--- js.js
+++ js.js
@@ -5,45 +5,53 @@ const URL1 = 'https://api.scryfall.com/cards/autocomplete?q='
 const URL2 = 'https://api.scryfall.com/cards/named?exact='
 nameArray = []
 
-
-
-
 print.addEventListener("click", loadDoc);
 
 async function loadDoc() {
     const response = await fetch(`${URL1}${send.value}`);
     const jsonData = await response.json();
     nameArray.push(jsonData)
-    console.log(jsonData);
-    console.log(nameArray)
     generateimg()
 }
-
 async function generateimg(){
+    console.log(nameArray[0].data)
     for ( objects of nameArray[0].data) {
-    console.log(objects)
     const response = await fetch(`${URL2}${objects}`);
     const cardData = await response.json();
-    console.log(cardData)
     if ('card_faces' in cardData){
-        const cards = document.createElement('div')
-        cards.classList.add('cards')
-        cards.innerHTML=`
-        <img class="frontFace" src=${cardData.card_faces[0].image_uris.normal}>
-        <img class="backSide" src=${cardData.card_faces[1].image_uris.normal}>
-        `
-        cardsprint.appendChild(cards)
-    } else {
-        const cards = document.createElement('div')
-        cards.classList.add('cards')
-        cards.innerHTML=`<img src=${cardData.image_uris.normal}>`
-        cardsprint.appendChild(cards)
+            const cards = document.createElement('div')
+            cards.classList.add('card')
+            cards.innerHTML=`<div class="doublefacedcard">
+                <img class="backSide" src=${cardData.card_faces[1].image_uris.normal}>
+                <img class="frontFace" src=${cardData.card_faces[0].image_uris.normal}>
+            </div><button class="flipbtn">flip</button>`
+            cardsprint.appendChild(cards)
+        } else {
+            const cards = document.createElement('div')
+            cards.classList.add('card')
+            cards.innerHTML=`<img src=${cardData.image_uris.normal}>`
+            cardsprint.appendChild(cards)
+        }
     }
-
-    }
+    setflip()
     clearArray()
+}
+
+function setflip(){
+    var flipButtons = document.querySelectorAll('.flipbtn')
+    flipButtons.forEach(btn=>{
+        btn.addEventListener('click',function(){
+            card = btn.parentElement.childNodes[0]
+            if(card.classList.contains('flip')){
+                card.classList.remove('flip')
+            }else{
+                card.classList.add('flip')
+            }
+        })
+    })
+
 }
 
 function clearArray(){
     nameArray.length = 0
-  }
+}
